50fac433baf97b109648ac5c24265c09f201504c
chengzi <26874039+chenchengzi@users.noreply.github.com>
2025-03-13 00:04:15 +0800

    测试提交

--- code/chapter-1/01-hello-pytorch.py
+++ code/chapter-1/01-hello-pytorch.py
@@ -12,4 +12,5 @@ print("Hello World, Hello PyTorch {}".format(torch.__version__))
 
 print("\nCUDA is available:{}, version is {}".format(torch.cuda.is_available(), torch.version.cuda))
 
+
 print("\ndevice_name: {}".format(torch.cuda.get_device_name(0)))
